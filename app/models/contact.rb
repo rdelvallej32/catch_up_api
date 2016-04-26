b1a63b3ac0f801f4e00b5aeddc321fbf05a90c90
rdelvallej32 <rdelvallej@gmail.com>
2016-04-25 23:16:24 -0400

    Add validation to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 # Contact that belongs to User
 class Contact < ActiveRecord::Base
   belongs_to :user, inverse_of: :contacts, class_name: 'User'
+  validates :user, presence: true
+  validates :user, uniqueness: true
 end
