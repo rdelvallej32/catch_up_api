758f919a72707e1918d578d0abb9c2e02d547927
rdelvallej32 <rdelvallej@gmail.com>
2016-05-01 15:19:24 -0400

    add reminder validation to contact serializer

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,5 +2,5 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :occupation,
              :professional_relationship, :company,
-             :last_contacted, :fact
+             :last_contacted, :fact, :reminder
 end
