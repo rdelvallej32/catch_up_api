f2eff08ea7c2e9daf3cb1ac435d3369d1a359a06
rdelvallej32 <rdelvallej@gmail.com>
2016-05-02 14:01:45 -0400

    change params for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,7 +61,7 @@ class UsersController < ProtectedController
 
   def user_creds
     params.require(:credentials)
-          .permit(:email, :password, :password_confirmation)
+          .permit(:email, :password, :password_confirmation, :username, :phone_number)
   end
 
   def pw_creds
