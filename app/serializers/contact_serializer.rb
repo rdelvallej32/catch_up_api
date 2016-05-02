164b14c215f7b4a5a47bd69ebbdc3af827f91b35
rdelvallej32 <rdelvallej@gmail.com>
2016-05-01 20:14:48 -0400

    add associations in contact and profile

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -3,4 +3,5 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :occupation,
              :professional_relationship, :company,
              :last_contacted, :fact, :reminder
+  belongs_to :user
 end
