84fe1a1f9ee3327e3bd81098f02b916a8b5adb19
rdelvallej32 <rdelvallej@gmail.com>
2016-06-21 19:01:18 -0400

    Add put in application config
    - When using ember-client, the put should work now

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,7 @@ module CatchUp
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
-                           :post, :patch, :delete]
+                           :post, :put, :patch, :delete]
       end
     end
   end
